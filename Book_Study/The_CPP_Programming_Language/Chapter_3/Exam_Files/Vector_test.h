84ba322613ec42f6812f64ec718d8cf88f4c6cac
AndrewPrograhams <ascotgraham@gmail.com>
2023-11-06 10:44:53 -0800

    More chapter 3 modifications.

--- Book_Study/The_CPP_Programming_Language/Chapter_3/Exam_Files/Vector_test.h
+++ Book_Study/The_CPP_Programming_Language/Chapter_3/Exam_Files/Vector_test.h
@@ -12,62 +12,66 @@ class Vector: public Container{
                 if(s<0) throw std::bad_array_new_length();
             }
 
+        double& operator[](int i) {
+            if(i<0 || i> static_cast<int>(size())) throw std::runtime_error("Vector operator[] cannot be less than 0 or greater than its size.");
+            return elem[i];
+        }
+
+        //const access operator
+        const double& operator[](int i) const
+        {
+            if(i < 0 || i > size()) throw std::out_of_range("Vector [] operation out of bounds.");
+            return elem[i];
+        }
+
+        Vector(Vector&& moved):
+            elem{moved.elem},
+            sz{moved.size()}
+        {
+                moved.elem = nullptr;
+                moved.sz = 0;
+        }
+        
+        Vector& operator=(Vector&& moved)
+        {
+            if(this!=&moved)
+            {
+                delete[] elem;
+                elem = moved.elem;
+                sz = moved.size();
+                moved.elem = nullptr;
+                moved.sz = 0;
+            }
+            return *this;
+        }
+        
+        // copy constructor
         Vector(const Vector& copy):
             elem{new double[copy.size()]},
             sz{copy.size()}
         {
             for(int i=0;i<copy.size();i++)
                 elem[i] = copy[i];
+            
         }
 
-        Vector(Vector&& moved):
-            elem{moved.elem},
-            sz{moved.sz}
+        // copy assignment
+        Vector& operator=(const Vector& copy)
+        {
+            if(this!=&copy)
             {
-                moved.elem = nullptr;
-                moved.sz = 0;
-            }
-
-        double& operator[](int i) {
-            if(i<0 || i> static_cast<int>(size())) throw std::runtime_error("Vector operator[] cannot be less than 0 or greater than its size.");
-            return elem[i];
-        }
-
-        const double& operator[](int i) const {
-            if(i<0 || i> static_cast<int>(size())) throw std::runtime_error("Vector operator[] cannot be less than 0 or greater than its size.");
-            return elem[i];
-        }
-
-        // copy assignment operator
-        Vector& operator=(const Vector& assigner){
-            if(this!=&assigner)
-            {
-                double* p = new double[assigner.size()];
-                for(std::size_t i=0; i<assigner.size();i++)
-                    p[i] = assigner[i];
+                double* p = new double[copy.size()];
+                for(int i=0; i<copy.size(); i++)
+                    p[i] = copy[i];
+                
                 delete[] elem;
                 elem = p;
-                sz = assigner.size();
+                sz = copy.size();
             }
             return *this;
         }
         
-        Vector& operator=(Vector&& moved) noexcept
-        {
-            if(this!=&moved)
-            {
-                delete[] elem;
 
-                elem = moved.elem;
-                sz = moved.size();
-                
-                moved.elem = nullptr;
-                moved.sz = 0;
-            }
-
-            return *this;
-        }
-        
         std::size_t size() const {return sz;}
 
         ~Vector(){
--- Book_Study/The_CPP_Programming_Language/Chapter_3/Exam_Solutions/Vector.h
+++ Book_Study/The_CPP_Programming_Language/Chapter_3/Exam_Solutions/Vector.h
@@ -29,6 +29,58 @@ class Vector{
         int size() const {return sz;}
         ~Vector() {delete elem;}
 
+                const double& operator[](int i) const {
+            if(i<0 || i> static_cast<int>(size())) throw std::runtime_error("Vector operator[] cannot be less than 0 or greater than its size.");
+            return elem[i];
+        }
+
+        // copy assignment operator
+        Vector& operator=(const Vector& assigner){
+            if(this!=&assigner)
+            {
+                double* p = new double[assigner.size()];
+                for(std::size_t i=0; i<assigner.size();i++)
+                    p[i] = assigner[i];
+                delete[] elem;
+                elem = p;
+                sz = assigner.size();
+            }
+            return *this;
+        }
+        
+        Vector& operator=(Vector&& moved) noexcept
+        {
+            if(this!=&moved)
+            {
+                delete[] elem;
+
+                elem = moved.elem;
+                sz = moved.size();
+                
+                moved.elem = nullptr;
+                moved.sz = 0;
+            }
+
+            return *this;
+        }
+
+        // Copy constructor
+        Vector(const Vector& copy):
+            elem{new double[copy.size()]},
+            sz{copy.size()}
+        {
+            for(int i=0;i<copy.size();i++)
+                elem[i] = copy[i];
+        }
+        // Move constructor
+        Vector(Vector&& moved):
+            elem{moved.elem},
+            sz{moved.sz}
+            {
+                moved.elem = nullptr;
+                moved.sz = 0;
+            }
+
     private:
         double* elem;
         std::size_t sz;
